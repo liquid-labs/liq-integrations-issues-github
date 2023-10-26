98f7e147e49dc7b60bcbba6b7031549478e6f907
Zane Rockenbaugh <zane@liquid-labs.com>
2023-10-26 16:21:54 -0500

    remove debug

--- src/get-qa-link-file-index.mjs
+++ src/get-qa-link-file-index.mjs
@@ -2,7 +2,6 @@ import { getGitHubOrgAndProjectBasename } from '@liquid-labs/github-toolkit'
 import { getGitHubQAFileLinks } from '@liquid-labs/liq-qa-lib'
 
 const getQALinkFileIndex = async({ app, pkgJSON, projectPath, reporter }) => {
-  console.log('getQALinkFileIndex pkgJSON:', pkgJSON) // DEBUG
   const { org: gitHubOrg } = getGitHubOrgAndProjectBasename({ packageJSON : pkgJSON })
 
   return await getGitHubQAFileLinks({ gitHubOrg, projectPath, reporter })
