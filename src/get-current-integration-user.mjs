acf85f8e68fbd61f519161722a55fbb8554e8d04
Zane Rockenbaugh <zane@liquid-labs.com>
2023-10-26 10:13:36 -0500

    three small updates: factor out 'org', regularize on 'projectBasename', and update 'CredentialsDB.getToken()' call

--- src/get-current-integration-user.mjs
+++ src/get-current-integration-user.mjs
@@ -2,7 +2,7 @@ import { determineGitHubLogin } from '@liquid-labs/github-toolkit'
 
 const getCurrentIntegrationUser = async({ app }) => {
   const credDB = app.ext.credentialsDB
-  const authToken = credDB.getToken('GITHUB_API')
+  const authToken = await credDB.getToken('GITHUB_API')
 
   // determine assignee(s)
   return (await determineGitHubLogin({ authToken })).login
